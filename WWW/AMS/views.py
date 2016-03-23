a15f58df606337a5fc00888473efbb62603accbc
kimkyeoungmin <kgm0219@gmail.com>
2016-03-23 16:08:24 +0900

    indent

--- WWW/AMS/views.py
+++ WWW/AMS/views.py
@@ -6,16 +6,16 @@ def post_list(request):
 	return render(request, 'blog/post_list.html', {})
 
 def index(request):
-    checkuser = request.user
-    username = request.username;
-    password = request.password;
-    user = auth.authenticate(username=username, password=password)
-    if user is not None:
-	 if user.is_active:
-		auth.login(request, user)
-		if checkuser.is_staff:
-			return render(request, 'AMS/professor.html', {})
-		else:
-			return render(request, 'AMS/student.html', {})
+	checkuser = request.user
+	username = request.username;
+	password = request.password;
+	user = auth.authenticate(username=username, password=password)
+	if user is not None:
+		if user.is_active:
+			auth.login(request, user)
+			if checkuser.is_staff:
+				return render(request, 'AMS/professor.html', {})
+			else:
+				return render(request, 'AMS/student.html', {})
 
 
