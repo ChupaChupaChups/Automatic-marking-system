5608797a2f5000019b36480f98ce349fc235c4d4
kimkyeoungmin <kgm0219@gmail.com>
2016-03-23 17:15:05 +0900

    add logout

--- WWW/AMS/views.py
+++ WWW/AMS/views.py
@@ -2,14 +2,13 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login, forms
 # Create your views here.
 
-def post_list(request):
-	return render(request, 'blog/post_list.html', {})
-
 def index(request):
 	checkuser = request.user
 	if checkuser.is_staff:
 		return render(request, 'AMS/professor.html', {})
 	else:
 		return render(request, 'AMS/student.html', {})
-
+def logout(request):
+	logout(request)
+	return render(request, '/', {}) 
 
