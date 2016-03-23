f9087e2ea913e6af8b2a850ae1dfbeb3d38c34c8
kimkyeoungmin <kgm0219@gmail.com>
2016-03-23 20:41:18 +0900

    auth

--- WWW/AMS/views.py
+++ WWW/AMS/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, forms, logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+@login_required
 def index(request):
 	checkuser = request.user
 	if checkuser.is_staff:
