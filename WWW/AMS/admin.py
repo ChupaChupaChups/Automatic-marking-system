8749b3e0339d50f63096a0b41ea2377fa0239f16
kimkyeoungmin <kgm0219@gmail.com>
2016-03-30 21:05:59 +0900

    register admin site

--- WWW/AMS/admin.py
+++ WWW/AMS/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Problem, Problem_condition
 # Register your models here.
+
+admin.site.register(Problem)
+admin.site.register(Problem_condition)
