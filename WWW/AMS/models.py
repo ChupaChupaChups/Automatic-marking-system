f708616acf8d3ad9ced7cb633a73dede67dd6cb7
eldsg <kgm0219@gmail.com>
2016-04-05 01:00:52 +0900

    djangodb

--- WWW/AMS/models.py
+++ WWW/AMS/models.py
@@ -2,22 +2,32 @@ from django.db import models
 
 # Create your models here.
 
-class Problem(models.Model):		
-	p_condition =  models.ForeignKey('Problem_condition')		# problem condition
+class Problem(models.Model):
+	upload_to = 'problem/testcase/%s'
+	
+	def _get_upload_to(instance, filename):
+		return instance.upload_to % (instance.p_name) 
+
+	p_day_limit = models.DateTimeField()
+	p_submissions_count = models.IntegerField(default=0)
+	p_c_ok = models.BooleanField()
+	p_cpp_ok = models.BooleanField()
+	p_java_ok = models.BooleanField()
+	p_py_ok = models.BooleanField()
+	p_hint_integer = models.IntegerField(default=100)
+	p_pwd = models.FileField(upload_to=_get_upload_to)
+	p_judge = models.BooleanField()
 	p_name = models.CharField(max_length=100)			# 문제 이름
 	p_content = models.TextField(null=False)			# 문제 내용
 	p_input = models.TextField(null=False)				# 입력 예제
 	p_output = models.TextField(null=False)				# 출력 예제
 
-class Problem_condition(models.Model):
-	day_limit = models.DateField()					# 제출기간 
-	day_over = models.BooleanField()				# 제출기간을 초과 하였는지
-	c_ok = models.BooleanField()					# C언어로 채점 가능 여부
-	cpp_ok = models.BooleanField()					# C++언어로 채점 가능 여부
-	java_ok = models.BooleanField()					# JAVA언어로 채점 가능 여부
-	py_ok = models.BooleanField()					# python언어로 채점 가능 여부
-	hint = models.BooleanField()					# 힌트 제공 여부
-	hint_integer = models.IntegerField()			  	# 힌트 제공 시 얼마나 문제를 맞춰야 힌트가 제공 되는지 숫자값
-	judge = models.BooleanField()					# True시 최적화 결과에 따라서 채점 False시 정답시 통과
-	submissions_count = models.IntegerField(default=0)		# 학생이 시도한 횟수
-	
+class Submit_record(models.Model):
+	submit_p_name = models.CharField(max_length=100)
+	submit_user_name = models.CharField()
+	submit_time = models.DateTimeField()
+	submit_result = models.BooleanField()
+	submit_correct_percent = models.IntegerField()
+	submit_language = models.IntegerField()				# 1 = c 2 = cpp 3 = java 4 = py
+	submit_use_time = models.IntegerField()				# 소요 시간
+
