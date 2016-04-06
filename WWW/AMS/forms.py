8d6a3365b8e142d5346a4bb5be81db61bba98470
kimkyeoungmin <kgm0219@gmail.com>
2016-04-06 15:17:04 +0900

    change name

--- WWW/AMS/forms.py
+++ WWW/AMS/forms.py
@@ -1,7 +1,7 @@
-from django import ModelForm
+from django.forms import ModelForm
 from AMS.models import Problem, Submit_record
 
-class Problem_add(ModelForm):
+class ProblemForm(ModelForm):
 	class Meta:
 		model = Problem
 		fields= ['p_day_limit', 'p_submissions_count', 'p_c_ok', 'p_cpp_ok', 'p_java_ok', 'p_py_ok', 'p_hint_integer', 'p_pwd', 'p_judge', 'p_name', 'p_content', 'p_input', 'p_output']
