c7042ca5ef3f2bef7fbf799261c1f79bbabca294
kimkyeoungmin <kgm0219@gmail.com>
2016-04-06 14:25:54 +0900

    return name

--- WWW/AMS/models.py
+++ WWW/AMS/models.py
@@ -22,6 +22,9 @@ class Problem(models.Model):
 	p_input = models.TextField(null=False)				# 입력 예제
 	p_output = models.TextField(null=False)				# 출력 예제
 
+	def __str__(self):
+		return self.p_name
+
 class Submit_record(models.Model):
 	submit_p_name = models.CharField(max_length=100)
 	submit_user_name = models.CharField(max_length=100)
@@ -30,4 +33,6 @@ class Submit_record(models.Model):
 	submit_correct_percent = models.IntegerField()
 	submit_language = models.IntegerField()				# 1 = c 2 = cpp 3 = java 4 = py
 	submit_use_time = models.IntegerField()				# 소요 시간
+	def __str__(self):
+		return submit_p_name
 
