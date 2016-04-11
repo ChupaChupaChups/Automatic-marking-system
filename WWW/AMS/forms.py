706eb4cd85d231496aae5ca1b35f3dcea93c7df4
kimkyeoungmin <kgm0219@gmail.com>
2016-04-11 15:33:18 +0900

    change code

--- WWW/AMS/forms.py
+++ WWW/AMS/forms.py
@@ -9,10 +9,8 @@ from .models import Problem
 
 class ProblemForm(forms.ModelForm):
 	p_content = forms.CharField(widget = CKEditorWidget())
+	p_day_limit = forms.DateTimeField(widget=DateTimeWidget(usel0n=True, bootstrap_version=3))
 	class Meta:
 		model = Problem
-		widgets = {
-			'p_day_limit': DateTimeWidget(attrs={'p_day_limit':"p_day_limit"}, usel10n=True, bootstrap_version=3)
-		}
 		fields = ['p_day_limit', 'p_submissions_count', 'p_c_ok', 'p_cpp_ok', 'p_java_ok', 'p_py_ok', 'p_hint_integer',
 				'p_infile', 'p_outfile', 'p_judge', 'p_name', 'p_content', 'p_input', 'p_output']
