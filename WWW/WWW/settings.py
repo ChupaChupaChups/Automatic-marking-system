d82b694308f870e3cdb5e6ab7111b068dec3eb1c
isac322 <isac322@naver.com>
2016-04-13 00:00:39 +0900

    Revert "add static files"
    This reverts commit 4e5ae74936e7c1a7b693fce36ad2bf83a42d8d41.

--- WWW/WWW/settings.py
+++ WWW/WWW/settings.py
@@ -126,5 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'statics')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 LOGIN_REDIRECT_URL = '/home/index'
