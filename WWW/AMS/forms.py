82958f5a527188f8d55d94ce938336db23e93348
isac322 <isac322@naver.com>
2016-04-13 00:56:08 +0900

    fix typo

--- WWW/AMS/forms.py
+++ WWW/AMS/forms.py
@@ -12,4 +12,4 @@ class ProblemForm(forms.ModelForm):
 			'p_content': SummernoteInplaceWidget(),
 		}
 		fields = ['p_day_limit', 'p_submissions_count', 'p_c_ok', 'p_cpp_ok', 'p_java_ok', 'p_py_ok', 'p_hint_integer',
-			'p_infile', 'p_outfile', 'p_judge', 'p_name', 'p_content', 'p_input', 'p_output']
+			'p_infile', 'p_outfile', 'p_judge', 'p_name', 'p_content', 'p_input', 'p_output', 'p_inputex', 'p_outputex']
