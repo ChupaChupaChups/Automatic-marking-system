435ac24e86333af01af3aa134e7cde7f164fa185
kimkyeoungmin <kgm0219@gmail.com>
2016-04-12 15:11:50 +0900

    delete env

--- WWW/env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/kyeounminkim/Desktop/KKM/Automatic-marking-system/Automatic-marking-system/WWW/env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
