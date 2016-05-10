a259f708573ac0b666fca1058487c9498f7d16e5
kimkyeoungmin <kgm0219@gmail.com>
2016-05-10 14:10:40 +0900

    findMainEntryPoint

--- WWW/AMS/static/assets/js/classfinder.js
+++ WWW/AMS/static/assets/js/classfinder.js
@@ -3,32 +3,41 @@ var x = document.getElementById("id_entry_point");
 var fileUploadBtn = document.getElementById("id_attachments");
 function readText() {
 	while (x.options.length) x.remove(0);
-
+	var isJava = document.getElementById("id_p_java_ok").checked;
 	console.log(fileUploadBtn.files);
-
-	for (var i = 0; i < fileUploadBtn.files.length; i++) {
-		var reader = new FileReader();
-
-		reader.onload = function (event) {
-			var contents = event.target.result;
-			var matches;
-
-			while (matches = regex.exec(contents)) {
-//				console.log(matches);
-				var option = document.createElement("option");
-				option.text = matches[1];
-				option.value = matches[1];
-				x.add(option);
-			}
-
-		};
-
-		reader.onerror = function () {
-			console.error("File could not be read!");
-		};
-
-//		console.log(fileUploadBtn.files[i]);
-		reader.readAsText(fileUploadBtn.files[i]);
+	if(isJava){
+		for (var i = 0; i < fileUploadBtn.files.length; i++) {
+			var reader = new FileReader();
+		
+			reader.onload = function (event) {
+				var contents = event.target.result;
+				var matches;
+	
+				while (matches = regex.exec(contents)) {
+//					console.log(matches);
+					var option = document.createElement("option");
+					option.text = matches[1];
+					option.value = matches[1];
+					x.add(option);
+				}
+	
+			};
+	
+			reader.onerror = function () {
+				console.error("File could not be read!");
+			};
+	
+//			console.log(fileUploadBtn.files[i]);
+			reader.readAsText(fileUploadBtn.files[i]);
+		}
+	}
+	else{
+		for (var i = 0; i < fileUploadBtn.files.length; i++){
+			var option = document.createElement("option");
+			option.text = fileUploadBtn.files.item(i).name;
+			option.value = fileUploadBtn.files.item(i).name;
+			x.add(option);
+		}
 	}
 }
 fileUploadBtn.addEventListener('change', readText, false);
