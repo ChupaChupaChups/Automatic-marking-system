2b8cfee63f3dcb208b109db5441d06e9243a9103
JaeheeSon <redpen709@naver.com>
2016-05-18 17:47:30 +0900

    progress bar to hint percentage

--- WWW/AMS/static/assets/js/problem_add.js
+++ WWW/AMS/static/assets/js/problem_add.js
@@ -22,5 +22,15 @@ document.addEventListener("DOMContentLoaded", function () {
 		if (event.target.checked) outputex.style.display = 'block';
 		else outputex.style.display = 'none';
 	});
+	document.getElementById('minus').addEventListener('click',function(event){
+		document.getElementById("hintBar").value -= 5;
+		if (document.getElementById("hintBar").value == 0) document.getElementById("percent").innerHTML = "항상 힌트를 보여줍니다";
+		else document.getElementById("percent").innerHTML = document.getElementById("hintBar").value + "% 이상 맞으면 힌트보여주기";
+	});
+	document.getElementById('plus').addEventListener('click',function(event){
+                document.getElementById("hintBar").value += 5;
+		if (document.getElementById("hintBar").value == 100) document.getElementById("percent").innerHTML = "힌트를 보여주지 않습니다";
+		else document.getElementById("percent").innerHTML = document.getElementById("hintBar").value + "% 이상 맞으면 힌트보여주기";
+	});
 
 });
