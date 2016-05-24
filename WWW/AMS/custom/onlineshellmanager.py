e60cb6fb3d7a395116c224415b233ae144479189
isac322 <isac322@naver.com>
2016-05-24 17:32:45 +0900

    fix typo

--- WWW/AMS/custom/onlineshellmanager.py
+++ WWW/AMS/custom/onlineshellmanager.py
@@ -10,7 +10,9 @@ import random
 
 current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parent_dir = os.path.dirname(current_dir)
-sys.path.insert(0, os.path.dirname(parent_dir))
+parent_dir = os.path.dirname(parent_dir)
+parent_dir = os.path.dirname(parent_dir)
+sys.path.insert(0, parent_dir)
 
 from judge_server.configuration.config import Config
 
