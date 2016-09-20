7e34c53423892cbab6cf1e353e58bb562875c8c7
nw <nameuk166@gmail.com>
2016-09-20 23:34:20 +0900

    changes python_compile to send multiple and dynamic inputfiles

--- WWW/AMS/judge_server/judge_scripts/run_python.sh
+++ WWW/AMS/judge_server/judge_scripts/run_python.sh
@@ -1,11 +1,35 @@
 #!/usr/bin/env bash
 
+entry=$(jq '.entry_point' /source_code/config.json)
+input_files=$(find /inputfiles -name "*.in")
+output_files=$(find /outputfiles -name "*.out")
+declare -i correct=0
+declare -i infilelen=0
+declare -i resulttime=0
+
 # TODO: 최종적으론 makefile로 변경
 
-# TODO: 테스트 파일 여러개 가능하도록 변경
-# TODO: 테스트 파일 이름을 고정이 아니도록 변경
 # TODO: entry point를
 
-entry=$(jq '.entry_point' /source_code/config.json)
+# (time python3 /source_code/$entry < /inputfiles/input.txt >> /source_code/result.txt) 2>&1 >/dev/null | tail -n 3 |head -1 | awk '{print $2}' | awk 'BEGIN {FS="[ms]"} {print ($1*60000+$2*1000)}' > /resultfiles/proctime.txt
 
-(time python3 /source_code/$entry < /inputfiles/input.txt >> /source_code/result.txt) 2>&1 >/dev/null | tail -n 3 |head -1 | awk '{print $2}' | awk 'BEGIN {FS="[ms]"} {print ($1*60000+$2*1000)}' > /resultfiles/proctime.txt
+
+for input_file in $input_files; do
+    infilelen=$infilelen+1
+    filename=$(basename $input_file)
+    (time python3 /source_code/$entry < $input_file > /resultfiles/${filename%.*}.out) 2>&1 >/dev/null | tail -n 3 |head -1 | awk '{print $2}' | awk 'BEGIN {FS="[ms]"} {print ($1*60000+$2*1000)}' > /resultfiles/${filename%.*}.time
+    temp=$(</resultfiles/${filename%.*}.time)
+    if [ $temp -gt $resulttime ];
+    then
+        resulttime=$temp
+    fi
+    for output_file in $output_files; do
+        if (cmp $output_file /resultfiles/${filename%.*}.out) 2>&1 >/dev/null ;
+        then
+            correct=$correct+1
+        fi
+    done
+done
+correct=$correct*100
+temp=$(($correct/$infilelen))
+python3 /compiler_and_judge/result_dump.py $temp $resulttime
